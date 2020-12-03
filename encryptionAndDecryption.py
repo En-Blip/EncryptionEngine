410d12972fff46a88c931b5052322f5162b90722
En-Blip <75442643+En-Blip@users.noreply.github.com>
2020-12-03 14:44:54 -0500

    Update encryptionAndDecryption.py

--- encryptionAndDecryption.py
+++ encryptionAndDecryption.py
@@ -4,7 +4,7 @@ alphabet=' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQR5STUVWXYZ1234567890'
 keynums='123456789'
 testNum=3
 
-print("\n+------------------------------------------------------+\n|     Noah 's Encryption and Decryption Algorithm      |\n|                Created Dec 3, 2020                   |\n|                                                      |\n|    For Eny Questions Email: contact@communism.com    |\n+------------------------------------------------------+\n\n\n\n")
+#print("\n+------------------------------------------------------+\n|     Noah 's Encryption and Decryption Algorithm      |\n|                Created Dec 3, 2020                   |\n|                                                      |\n|    For Eny Questions Email: contact@communism.com    |\n+------------------------------------------------------+\n\n\n\n")
 
 def encryption(message, encryptionLevel):
     keySignature = "Key Signature: _"
